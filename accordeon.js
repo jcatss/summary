34f8aeb012fe9bd59f087ca75d593b8e7c0688f4
jcatss <snieznapanda@gmail.com>
2023-03-01 08:17:45 +0200

    last update summary 01.03.2023

--- accordeon.js
+++ accordeon.js
@@ -1,14 +1,13 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + 600 + "px";
-    } 
+$(document).ready(function () {
+  $(".notification").slideUp();
+  $(function(){
+    $(".p1").click(function () {
+      $(".n1").slideToggle();
+    });
   });
-}
+  $(function(){
+    $(".p2").click(function () {
+      $(".n2").slideToggle();
+    });
+  });
+});
